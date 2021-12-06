051126584b9710ab6bca46e5b2191ce3b2645594
nathaliabeatriz <94134475+nathaliabeatriz@users.noreply.github.com>
2021-12-06 15:11:37 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuHamburger = document.getElementById('menu-hamburger');
-
-function menuAparece(){
-    const nav = document.getElementById('navegar2');
-    nav.classList.toggle('active');
-}
-
-menuHamburger.addEventListener('click', menuAparece);
